de3a2589016ffb4b621b72c5b7e9e8a03e08a94e
Melinda Swartzfager <melinda@melindas-macbook-pro.local>
2018-02-10 17:21:56 -0800

    Initial Commit

---
+++ FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 31/8/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var headerView: RestaurantDetailHeaderView!
+    
+    // MARK: - Properties
+    
+    var restaurant: Restaurant = Restaurant()
+    
+    // MARK: - View controller life style
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        // Configure header view
+        
+        headerView.nameLabel.text = restaurant.name
+        headerView.typeLabel.text = restaurant.type
+        headerView.headerImageView.image = UIImage(named: restaurant.image)
+        headerView.heartImageView.isHidden = (restaurant.isVisited) ? false : true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by Melinda Swartzfager on 2/10/18.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    @IBOutlet var headerImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var heartImageView: UIImageView!
+
+}
